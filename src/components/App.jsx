0fe9663eb5bee38892d798bde33f93fd76195480
thismax <max.g.jacobs@gmail.com>
2017-09-05 15:06:08 -0700

    able to get messages from server

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 import Send from './Send';
 import Messages from './Messages'
 
-class App extends {
+class App extends Component {
 
 	constructor (props) {
 		super(props);
 		this.state = {
-			messages = [];
+			messages: [],
 		}
 
+		this.postRequest = this.postRequest.bind(this);
 		this.handleSend = this.handleSend.bind(this);
 		this.getMessages = this.getMessages.bind(this);
+
+	}
+
+	getMessages () {
+		axios.get('http://chattercube.thirdtape.com/messages')
+			.then(({data}) => { 
+				console.log(data);
+			})
+			.catch(err => console.log(err));
+	}
+
+	componentDidMount() {
+		this.getMessages();
+	}
+
+	postRequest () {
+
 	}
 
 	handleSend () {
 
 	}
 
-	getMessages () {
-
-	}
-
 	render () {
-		const App = () => (
-				<div>
-					<Search />
-					<Messages />
-				</div>
-			);
-		}
+		return (
+			<div>
+				test
+			</div>
+		);
 	}
 
-
+}
 
 export default App;
