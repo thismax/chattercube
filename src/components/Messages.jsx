946c77e6e09255ecca4dfb8f248d483c5d9c708c
thismax <max.g.jacobs@gmail.com>
2017-09-05 21:01:25 -0700

    fixed tab issue

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import Message from './Message';
 
 const Messages = props => (
-  <div className="messages">
-    {props.messages.map((message, i) =>
-      (<Message
-        key={i}
-        username={message.username}
-        message={message.message}
-      />),
-    )}
-  </div>
+	<div className="messages">
+		{props.messages.map((message, i) =>
+			(<Message
+				key={i}
+				username={message.username}
+				message={message.message}
+			/>),
+		)}
+	</div>
 );
 
 export default Messages;
