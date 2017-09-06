963ae22cc12c74150a8c36a24647cb6fac9fa2c4
thismax <max.g.jacobs@gmail.com>
2017-09-05 21:00:30 -0700

    fixed tab issue

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types'; 
 	
 	const Message = props => (
-	  <div className="message">
-	    <span className="username">{props.username}: </span>
-	    <span>{props.message}</span>
-	  </div>
+		<div className="message">
+			<span className="username">{props.username}: </span>
+			<span>{props.message}</span>
+		</div>
 	);
 
 	Message.propTypes = {
-	  username: PropTypes.string.isRequired,
-	  message: PropTypes.string.isRequired,
+		username: PropTypes.string.isRequired,
+		message: PropTypes.string.isRequired,
 	};
 
 export default Message;
